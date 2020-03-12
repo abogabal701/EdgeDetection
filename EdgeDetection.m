b8fe02c42ce774dec1e6234d6d6f8ef0336de822
Ahmed Algebaly <36502794+abogabal701@users.noreply.github.com>
2020-03-12 23:17:30 +0200

    Add files via upload

---
+++ EdgeDetection.m
@@ -0,0 +1,13 @@
+I = imread('m83.tif');
+figure;
+imshow(I); %show original image
+firstDev = edge(I, 'prewitt');
+figure;
+imshow(firstDev); % show edges with first derivative
+secondDev = edge(I, 'log');
+figure;
+imshow(secondDev); % show edges with second derivative
+cannyAlg = edge(I, 'canny');
+figure;
+imshow(cannyAlg); % show edges with canny algorithm
+
